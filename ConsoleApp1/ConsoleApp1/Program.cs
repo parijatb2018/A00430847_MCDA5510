aeacf31f7ad8ba18ed7960c6982e6f3eb8b44f1b
parijatb2018 <43022026+parijatb2018@users.noreply.github.com>
2018-11-04 18:24:47 -0400

    ConsoleApp1 modified version
    Country and street added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,147 @@
+﻿//  File ContactGenerator
+//  Sample code was taken from:
+//  http://www.csharpprogramming.tips/2013/06/RandomDoxGenerator.html
+//  Other useful methods are there.
+//
+// Requirements:
+// Exapand on the below example to create a CSV file (https://en.wikipedia.org/wiki/Comma-separated_values)
+// For contacts with the following data
+// First Name
+// Last Name
+// Street Number
+// City
+// Province
+// Country  == Canada ( Simply insert "canada")
+// Postal Code  ( they can be read form a file for this example if you choose, or generate if you wish)
+// Phone Number ( they can be read form a file for this example if you choose, or generate if you wish)
+// email Address ( Append firstname.lastname against a series for domain names read for a file
+//
+// Please always try to write clean and readable code
+// Here is a good reference doc http://ricardogeek.com/docs/clean_code.html  
+// Submit to Bitbucket under Assignment1
+
+// 
+
+using System;
+using System.IO;
+
+// Describes what is a namespace 
+// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/namespaces/
+namespace MSCDA5510
+{
+    class ContactGenerator
+    {
+        // instance of random number generator
+        Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            // instance of ContactGenerator
+            ContactGenerator dg =  new ContactGenerator();
+        }
+
+        public ContactGenerator()
+        {
+            String COMMA = ",";
+            String outputFileName = @"C:\Users\Parijat\Documents\Git\A00430847_MCDA5510\ConsoleApp1\ConsoleApp1\customers.csv";
+
+            if (File.Exists(outputFileName))
+            {
+                Console.Write(" File " + outputFileName + " exists, appending");
+            }
+            StreamWriter fileStream = new StreamWriter(outputFileName, true);
+
+            // Write Header
+            fileStream.Write("First Name");
+            fileStream.Write(COMMA);
+            fileStream.Write("Last Name");
+            fileStream.Write(COMMA);
+            fileStream.Write("counrty");
+            fileStream.Write(COMMA);
+            fileStream.Write("street");
+            fileStream.WriteLine();
+            //fileStream.WriteLine();
+
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                fileStream.Write(GenerateFirstName());
+                fileStream.Write(COMMA);
+                fileStream.Write(GenerateLastName());
+                //fileStream.WriteLine();
+                fileStream.Write(COMMA);
+                fileStream.Write(Generatecountry());
+                fileStream.Write(COMMA);
+                fileStream.Write(Generatestreet());
+                fileStream.WriteLine();
+            }
+            fileStream.Close();
+        }
+
+        public string Generatecountry()
+        {
+            String country = @"C:\Users\Parijat\Documents\Git\A00430847_MCDA5510\ConsoleApp1\ConsoleApp1\country.txt";
+            return ReturnRandomLine(country);
+        }
+
+        public string Generatestreet()
+        {
+            String street = @"C:\Users\Parijat\Documents\Git\A00430847_MCDA5510\ConsoleApp1\ConsoleApp1\street.txt";
+            return ReturnRandomLine(street);
+        }
+
+
+        public string GenerateFirstName()
+        {
+            String firstNames = @"C:\Users\Parijat\Documents\Git\A00430847_MCDA5510\ConsoleApp1\ConsoleApp1\firstNames.txt";
+            return ReturnRandomLine(firstNames);
+        }
+
+        public string GenerateLastName()
+        {
+            String lastNames = @"C:\Users\Parijat\Documents\Git\A00430847_MCDA5510\ConsoleApp1\ConsoleApp1\lastNames.txt";
+            return ReturnRandomLine(lastNames);
+        }
+
+        // Gets a line from a file
+        public string ReturnRandomLine(string FileName)
+        {
+            string sReturn = string.Empty;
+
+            using (FileStream myFile = new FileStream(FileName, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader myStream = new StreamReader(myFile))
+                {
+
+                    // just cast it to int because we know it will be less than 
+                    int fileLength = (int)myFile.Length;
+
+                    // Seek file stream pointer to a rand position...
+                    myStream.BaseStream.Seek(rand.Next(1,fileLength), SeekOrigin.Begin);
+
+                    //myStream.BaseStream.Seek(rand.Next(0,fileLength), SeekOrigin.Begin);
+
+                    // Read the rest of that line.
+                    myStream.ReadLine();
+
+                    // Return the next, full line...
+                    sReturn = myStream.ReadLine();
+                }
+            }
+
+            // If our random file position was too close to the end of the file, it will return an empty string
+            // I avoided a while loop in the case that the file is empty or contains only one line
+            if (System.String.IsNullOrWhiteSpace(sReturn))
+            {
+                sReturn = ReturnRandomLine(FileName);
+            }
+
+            return sReturn;
+        }
+    }
+
+
+
+
+}
